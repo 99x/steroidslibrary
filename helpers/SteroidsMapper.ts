0d629d891d90e338a19535d6edb6cab764b92e7f
Rifhan Akram <rifhan.akram1@gmail.com>
2017-09-14 17:26:13 +0530

    - fixes issue #9

--- helpers/SteroidsMapper.ts
+++ helpers/SteroidsMapper.ts
@@ -37,6 +37,22 @@ export class AppendOps{
         
         return this._mapper;
     }
+
+    async fromAsync(appendFunc: Function) {
+        if (appendFunc === undefined)
+            throw new SteroidMapperException("Steroids mapper doesn't accept null values for the from() function, should be a function");
+
+        if (this._obj instanceof Array){
+            let asyncArr = [];
+            for (let i=0;i<this._obj.length;i++)
+                asyncArr.push(this.appendObject(this._obj[i], appendFunc));
+            
+            await Promise.all(asyncArr);
+        }else
+            await this.appendObject(this._obj, appendFunc);
+        
+        return this._mapper;
+    }
 }
 
 export class MapOps{

2d0fc29804a17f733dc0ad2280cbcc286a62ebe9
supuncodes <supuncodes@gmail.com>
2017-10-12 11:29:06 +0530

    Latest changes

--- plugins/data/relational/SequelizeQueryExecutor.ts
+++ plugins/data/relational/SequelizeQueryExecutor.ts
@@ -26,20 +26,24 @@ export class SequelizeQueryExecutor implements IRelationalDatabase {
 
     executeQuery<T>(type: {new(): T;}, query: string): Promise<T[]> {       
         return new Promise<T[]>((resolve,reject)=>{
-            let sequelize = this.getConnection();
-            sequelize.query(query)
-            .then(results=>{
-                let val;
-                if (results.length > 0)
-                    val = results[0];
-                
-                this.convertToCamelCase(val);
+            this.getConnection().then((sequelize)=>{
+                sequelize.query(query)
+                .then(results=>{
+                    let val;
+                    if (results.length > 0)
+                        val = results[0];
+                    
+                    this.convertToCamelCase(val);
 
-                resolve(val);
-            })
-            .catch ((error)=>{
-                reject(error);
+                    resolve(val);
+                })
+                .catch ((error)=>{
+                    reject(error);
+                });
+            }).catch((err)=>{
+                reject(err);
             });
+
             /*
             .spread((results, metadata) => {
                 resolve(results);
@@ -48,33 +52,55 @@ export class SequelizeQueryExecutor implements IRelationalDatabase {
         });       
     }
 
-    private getConnection(){
-        if (this._connection)
-            return this._connection;            
-        else {
-            let Sequelize = require("sequelize");
-            let {driver, database, host, username, password, pool, dialectOptions} = this._config;
-            this._connection = new Sequelize(database, username, password, {
-                host: host,
-                dialect: driver,
-                pool: pool,
-                dialectOptions: dialectOptions
-            });
-            return this._connection;
-        }
+    private establishConnection(resolve,reject){
+        let Sequelize = require("sequelize");
+        let {driver, database, host, username, password, pool, dialectOptions} = this._config;
+        this._connection = new Sequelize(database, username, password, {
+            host: host,
+            dialect: driver,
+            pool: pool,
+            dialectOptions: dialectOptions
+        });
+        resolve(this._connection);
+    }
+
+    private getConnection(): Promise<any>{
+        var self = this;
+        return new Promise<any>((resolve,reject)=>{
+
+            let connectionPerRequest = this._config.connectionPerRequest ? this._config.connectionPerRequest : true;
+
+            if (connectionPerRequest){
+                this.establishConnection(resolve,reject);
+            }else {
+                if (self._connection){
+                    self._connection.authenticate().then(()=>{
+                        resolve(self._connection);
+                    }).catch((err)=>{
+                        this.establishConnection(resolve,reject);
+                    });
+                }
+                else 
+                    this.establishConnection(resolve,reject);
+            }
+
+        });
     }
 
     getDataSet(retrievalPlan:IRetrievalPlan[], inputSet?: Object):Promise<any> {
         let responseDataSet = {};
-        let sequelize = this.getConnection();
         return new Promise<any>((resolve,reject)=>{
-            this.executeRetrievalUnit(retrievalPlan,responseDataSet, inputSet ? inputSet : {},sequelize)
-            .then((result)=>{
-                resolve(responseDataSet);
-            })
-            .catch((err)=>{
+            this.getConnection().then((sequelize)=>{
+                this.executeRetrievalUnit(retrievalPlan,responseDataSet, inputSet ? inputSet : {},sequelize)
+                .then((result)=>{
+                    resolve(responseDataSet);
+                })
+                .catch((err)=>{
+                    reject(err);
+                });
+            }).catch((err)=>{
                 reject(err);
-            })
+            });
         })
         
     }

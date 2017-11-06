ab4c1eede5d9a4b62c5035948e302ab0447fedc8
supuncodes <supuncodes@gmail.com>
2017-11-06 15:07:10 +0530

    Improvements in steroids

--- plugins/storage/FileStorageFactory.ts
+++ plugins/storage/FileStorageFactory.ts
@@ -2,11 +2,28 @@ import {IFileStorage} from "./IFileStorage";
 import {QueryExecutorFileStorage} from "./QueryExecutorFileStorage";
 import {Steroid} from "../../Steroids";
 
+import {ValueRetriever} from "../../helpers/ValueRetriever"
+import {PluginManager} from "../../plugins/PluginManager"
+import {PluginType} from "../../plugins/PluginType"
+
 export class FileStorageFactory {
     public static create(steroid:Steroid) : IFileStorage {
         let outObject = Steroid.getMockObject("storage.file");
-        if (outObject === undefined)
-            outObject = new QueryExecutorFileStorage(steroid);
+        
+        if (outObject === undefined){
+            let config = steroid.config;
+            let storageName = ValueRetriever.getValue(config,"storage.type");
+
+            switch (storageName){
+                case "documentDownloader":
+                    outObject = new QueryExecutorFileStorage(steroid);
+                    break;
+                default:
+                    outObject = PluginManager.getInstance(steroid, PluginType.Storage, storageName);
+                    break;
+            }
+            
+        }
         return outObject;
     }
 }

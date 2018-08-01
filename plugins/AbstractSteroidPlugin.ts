cc21eb99e93c4d582cf549e3597cc07cfc855941
SERANET\SudarakaA <sudarakaa@99x.lk>
2018-08-01 14:09:36 +0530

    implementation

--- plugins/AbstractSteroidPlugin.ts
+++ plugins/AbstractSteroidPlugin.ts
@@ -4,5 +4,10 @@ import {Steroid} from "../Steroids"
 export abstract class AbstractSteroidPlugin {
     public abstract onRegister(): ISteroidPlugin;
 
-    public abstract getInstance(steroid:Steroid):any;
+    public abstract getInstance(steroid:Steroid, parameters?:any):any;
+
+    public parameters;
+    public setParameters (parameters) {
+        this.parameters = parameters;
+    }
 }
